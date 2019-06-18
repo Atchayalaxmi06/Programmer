a355cb18ad7eb4bead5dc44a244d73b50a75e525
Atchayalaxmi06 <51912583+Atchayalaxmi06@users.noreply.github.com>
2019-06-18 16:31:49 +0530

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,6 +1,7 @@
-yab=raw_input()
-vol={'a','e','i','o','u'}
-cot={'b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z'}
+yab=input()
+vol={'a','e','i','o','u' or 'A','E','I','O','U'}
+cot={'b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z' or
+     'B','C','D','F','G','H','J','K','L','M','N','P','Q','R','S','T','V','W','X','Y','Z'}
 if yab in vol:
   print("Vowel")
 elif yab in cot:
