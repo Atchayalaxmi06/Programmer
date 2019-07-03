2f9775ff3ee9befe1e1e9289355d0e6b9e180980
Atchayalaxmi06 <51912583+Atchayalaxmi06@users.noreply.github.com>
2019-07-03 23:09:26 +0530

    Create 16.py

---
+++ 16.py
@@ -0,0 +1,14 @@
+nn=input()
+nn=nn.split()
+st=int(nn[0])
+en=int(nn[1])
+for num in range(st+1,en):
+  if num>1:
+    for in range(2,num):
+      if(num%i==0):
+        break
+    else:
+        print(num,end=' ')
+    
+    
+  
